62473572946d255add0868b6c2c259b2e5ccc759
darman <dxlacerate@gmail.com>
2024-02-28 01:50:56 +0700

    delete all the repo

--- lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
-local keymap = vim.keymap
-
---directory
-keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>", { noremap = true, silent = true})
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true})
-
-
