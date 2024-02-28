5f48f2894b8a80fd56daf8d493ac9d3b1af0f1a3
darman <dxlacerate@gmail.com>
2024-02-29 05:57:48 +0700

    chore: adding diagnostic toggle for lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,7 +22,35 @@ return {
     cmd = {"LspInfo", "LspInstall", "LspUninstall", "Mason"},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local diagnostics_active = true
+      vim.keymap.set('n', '<leader>D', ':DiagnosticsToggle<CR>', {noremap = true, silent = true})
+      -- Command to toggle inline diagnostics
+    vim.api.nvim_create_user_command(
+      'DiagnosticsToggleVirtualText',
+        function()
+          local current_value = vim.diagnostic.config().virtual_text
+        if current_value then
+          vim.diagnostic.config({virtual_text = false})
+        else
+          vim.diagnostic.config({virtual_text = true})
+      end
+    end,
+  {}
+)
 
+      -- Command to toggle diagnostics
+      vim.api.nvim_create_user_command(
+      'DiagnosticsToggle',
+        function()
+          local current_value = vim.diagnostic.is_disabled()
+            if current_value then
+          vim.diagnostic.enable()
+            else
+          vim.diagnostic.disable()
+      end
+  end,
+  {}
+)
 			local lspconfig = require("lspconfig")
       require("lspconfig").eslint.setup {
         on_attach = function()
