8ef9c8e5d32d28ba35f5a31c7e21572f957925bc
darman <dxlacerate@gmail.com>
2024-03-01 07:38:59 +0700

    chore: change comment keys

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,10 +1,12 @@
 -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 return {
-    'numToStr/Comment.nvim',
-    lazy = false,
-    opts = {
-        -- add any options here
-    },
-    lazy = false,
+	"numToStr/Comment.nvim",
+	keys = {
+		{ "gcc", mode = "n", desc = "Comment toggle current line" },
+		{ "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
+		{ "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
+		{ "gbc", mode = "n", desc = "Comment toggle current block" },
+		{ "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+		{ "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+	},
 }
-
